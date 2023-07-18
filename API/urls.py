51d5c9096a122aa1cadcbf4d1b76c282feb57c63
marouane <marouane@marouanes-MacBook-Pro.local>
2023-07-18 21:57:28 +0100

    fix typo

--- API/urls.py
+++ API/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('keywords/update/', views.update_keywords, name='update_keywords'),
     path('users/', views.get_users, name='get_users'),
     path('users/update/', views.update_users, name='update_users'),
-    path('serverStatus/', views.get_server_status, name='get_server_statu'),
+    path('serverStatus/', views.get_server_status, name='get_server_status'),
     path('serverStatus/update/', views.update_server_status, name='update_server_status')
 ]
